f9b9bde18a2c80249926e5babbd10e2372057593
Mystic Kitty <djabranton@gmail.com>
2021-01-11 21:55:22 -0800

    Updated connection.js to include Jaws.  Added .env for config pw's.

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,18 @@
-// import the Sequelize constructor from the library
 const Sequelize = require('sequelize');
 
-// create connection to our database, pass in your MySQL information for username and password
-const sequelize = new Sequelize('adventure_series_db', 'username', 'password', {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
-});
+require('dotenv').config();
+
+// create connection to our db
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3306
+    });
+}
 
 module.exports = sequelize;
