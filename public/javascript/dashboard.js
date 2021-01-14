5ba43fa8e168223de68efe91919852c69f9bdd18
Mystic Kitty <djabranton@gmail.com>
2021-01-14 00:53:36 -0800

    Created most of dashboard form fields.

--- public/javascript/dashboard.js
+++ public/javascript/dashboard.js
@@ -1,8 +1,3 @@
 //Set calendar date in dashboard submit route form to today.
 let today = new Date().toISOString().substr(0, 10);
 document.querySelector("#today").value = today;
-
-
-// $(document).ready(function () {
-//     $('input.bs-timepicker').timepicker();
-// });
