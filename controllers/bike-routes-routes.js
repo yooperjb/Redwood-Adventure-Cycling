9d93623269b4753f08356f8513e06e869c082912
Calista Mayer <calista.a.mayer@gmail.com>
2021-01-16 12:06:24 -0800

    render routes on bikeroutes page

--- controllers/bike-routes-routes.js
+++ controllers/bike-routes-routes.js
@@ -1,7 +1,28 @@
+const { Routes } = require('../models');
+
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-    res.render('bikeroutes');
+    Routes.findAll({
+        // Query configuration
+        attributes: [
+            'name',
+            'mileage',
+            'elevation',
+            'points',
+            'first_bonus',
+            'difficulty',
+            'map',
+            'note'
+        ]
+    }).then(dbRoutesData => {
+        const routes = dbRoutesData.map(route => route.get({ plain: true }));
+        res.render('bikeroutes', { routes });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 });
 
 module.exports = router;
