6331fe713996c3f9c8567d76ff83f5bd3fd4523d
Mystic Kitty <djabranton@gmail.com>
2021-01-16 09:27:40 -0800

    pushing to develop

--- controllers/bike-routes-routes.js
+++ controllers/bike-routes-routes.js
@@ -1,34 +1,7 @@
 const router = require('express').Router();
-const { Routes } = require('../models');
-const sequelize = require('../../config/connection');
 
 router.get('/', (req, res) => {
-    Routes.findAll()
-        .then(routesData => res.json(routesData))
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        });
-})
-
-    .then(routesData => {
-        // serialize data before passing to template
-        const routes = routesData.map(routes => routes.get({ plain: true }));
-        res.render('dashboard', { routes });
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    });
-
-// router.get('/', (req, res) => {
-//     res.render('bikeroutes');
-// })
-//     .catch(err => {
-//         console.log(err);
-//         res.status(500).json(err);
-//     });
-
-
+    res.render('bikeroutes');
+});
 
 module.exports = router;
