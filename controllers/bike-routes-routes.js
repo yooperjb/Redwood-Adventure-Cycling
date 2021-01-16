9d680d857281bdd82104ac46f9aae4b76849d2f7
Calista Mayer <calista.a.mayer@gmail.com>
2021-01-15 18:13:07 -0800

    cycle handlebars

--- controllers/bike-routes-routes.js
+++ controllers/bike-routes-routes.js
@@ -1,7 +1,16 @@
 const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Routes } = require('../models');
 
 router.get('/', (req, res) => {
-    res.render('bikeroutes');
+    res.render('bikeroutes', {
+        name: 'test_route',
+        difficulty: 'Medium',
+        mileage: 33.22,
+        elevation: 7777.77,
+        points: 56,
+        first_bonus: 5
+    });
 });
 
 module.exports = router;
