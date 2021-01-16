4b8fe7614b93220aff8bb7d1187e7cfc10909d44
Mystic Kitty <djabranton@gmail.com>
2021-01-16 00:58:05 -0800

    Trying to get bike routes to display.

--- controllers/bike-routes-routes.js
+++ controllers/bike-routes-routes.js
@@ -1,7 +1,34 @@
 const router = require('express').Router();
+const { Routes } = require('../models');
+const sequelize = require('../../config/connection');
 
 router.get('/', (req, res) => {
-    res.render('bikeroutes');
-});
+    Routes.findAll()
+        .then(routesData => res.json(routesData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+})
+
+    .then(routesData => {
+        // serialize data before passing to template
+        const routes = routesData.map(routes => routes.get({ plain: true }));
+        res.render('dashboard', { routes });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+
+// router.get('/', (req, res) => {
+//     res.render('bikeroutes');
+// })
+//     .catch(err => {
+//         console.log(err);
+//         res.status(500).json(err);
+//     });
+
+
 
 module.exports = router;
