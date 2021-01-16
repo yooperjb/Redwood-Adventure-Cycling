bb6a025ec7828ce396ff8cb1f12832144555a963
Calista Mayer <calista.a.mayer@gmail.com>
2021-01-16 12:13:14 -0800

    render bike routes page

--- controllers/bike-routes-routes.js
+++ controllers/bike-routes-routes.js
@@ -5,15 +5,6 @@ const sequelize = require('../config/connection');
 const { Routes } = require('../models');
 
 router.get('/', (req, res) => {
-<<<<<<< HEAD
-    res.render('bikeroutes', {
-        name: 'test_route',
-        difficulty: 'Medium',
-        mileage: 33.22,
-        elevation: 7777.77,
-        points: 56,
-        first_bonus: 5
-=======
     Routes.findAll({
         // Query configuration
         attributes: [
@@ -33,7 +24,6 @@ router.get('/', (req, res) => {
     .catch(err => {
         console.log(err);
         res.status(500).json(err);
->>>>>>> feature/cycle-routes
     });
 });
 
