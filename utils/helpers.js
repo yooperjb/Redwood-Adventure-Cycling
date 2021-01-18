ffeeca80ed6e0ae10bd652756019ff01de70549a
Mystic Kitty <djabranton@gmail.com>
2021-01-17 23:51:31 -0800

    Updated guidelines, homepage and other minor css fixes.

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,34 +1,9 @@
-// // module.exports = function (handlebars) {
-// //     return function () {
-// //         return new handlebars.SafeString('Hello!');
-// //     };
-// // }
+module.exports = {
+    format_plural: (word, amount) => {
+        if (amount !== 1) {
+            return `${word}s`;
+        }
 
-
-// Handlebars.registerHelper('ifCond', function (v1, operator, v2, options) {
-
-//     switch (operator) {
-//         case '==':
-//             return (v1 == v2) ? options.fn(this) : options.inverse(this);
-//         case '===':
-//             return (v1 === v2) ? options.fn(this) : options.inverse(this);
-//         case '!=':
-//             return (v1 != v2) ? options.fn(this) : options.inverse(this);
-//         case '!==':
-//             return (v1 !== v2) ? options.fn(this) : options.inverse(this);
-//         case '<':
-//             return (v1 < v2) ? options.fn(this) : options.inverse(this);
-//         case '<=':
-//             return (v1 <= v2) ? options.fn(this) : options.inverse(this);
-//         case '>':
-//             return (v1 > v2) ? options.fn(this) : options.inverse(this);
-//         case '>=':
-//             return (v1 >= v2) ? options.fn(this) : options.inverse(this);
-//         case '&&':
-//             return (v1 && v2) ? options.fn(this) : options.inverse(this);
-//         case '||':
-//             return (v1 || v2) ? options.fn(this) : options.inverse(this);
-//         default:
-//             return options.inverse(this);
-//     }
-// });
+        return word;
+    }
+}
