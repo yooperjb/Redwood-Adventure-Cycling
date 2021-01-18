c2616fafe067c7b7e39eddfee8e1f367bb89d762
Pedal <jmb181@humboldt.edu>
2021-01-18 00:57:38 -0800

    admin approval form started

--- public/javascript/admin.js
+++ public/javascript/admin.js
@@ -1,22 +1,36 @@
-async function editFormHandler(event) {
+
+
+async function approvalFormHandler(event) {
     event.preventDefault();
-    // const id = window.location.toString().split('/')[
-    //     window.location.toString().split('/').length - 1
-    // ];
-    const response = await fetch(`/user_routes/${id}`, {
+    
+    const container = document.querySelector('.approve-form');
+    const approve = container.querySelectorAll('input, checkbox');
+    //console.log("approve",approve);
+    //console.log(typeof(approve));
+
+    ids = [];
+    
+    approve.forEach(route => {
+        if (route.checked){
+            ids.push(parseInt(route.value));
+        }
+    });
+
+    console.log("ids",ids)
+
+    const response = await fetch('/api/user_routes/', {
         method: 'PUT',
-        body: JSON.stringify({
-            approved,
-        }),
+        body: ids,
+        
         headers: {
             'Content-Type': 'application/json'
         }
     });
     if (response.ok) {
-        document.location.replace('/dashboard');
+        document.location.reload();
     } else {
         alert(response.statusText);
     }
 }
 
-document.querySelector('.approve-form').addEventListener('submit', editFormHandler);
+document.querySelector('.approve-form').addEventListener('submit', approvalFormHandler);
