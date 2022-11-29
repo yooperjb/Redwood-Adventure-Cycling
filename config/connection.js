867ce3c4fcb8f612fb877ee2cb64d0759c4cc635
Pedal <jmb181@humboldt.edu>
2022-11-28 19:50:23 -0800

    more front-end cleanup

--- config/connection.js
+++ config/connection.js
@@ -6,15 +6,22 @@ require('dotenv').config();
 // create connection to our db
 let sequelize;
 
-// Sets up sequelize for JAWSDB or Localhost
+// Sets up sequelize for JAWSDB (Heroku) or Localhost
 if (process.env.JAWSDB_URL) {
     sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
+} else if (process.env.STATUS === 'development' ){
     // If using local db - use .env variables to keep private
     sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
         host: 'localhost',
         dialect: 'mysql',
         port: 3306
+    });
+} else {
+    // For production site on Dreamhost process.env.STATUS === 'production'
+    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+        host: 'add this',
+        dialect: 'mysql',
+        port: 'addthis'
     });
 }
 
