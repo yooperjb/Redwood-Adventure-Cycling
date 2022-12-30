797c2d637ccef5ac60fd1d55162e895ccadc6ef6
Pedal <jmb181@humboldt.edu>
2022-12-29 21:07:00 -0800

    created all modals for dashboard route submit form

--- public/javascript/modal.js
+++ public/javascript/modal.js
@@ -1,26 +1,36 @@
 // Get the Question Mark that opens the modal
-const qMark = document.getElementById("qMark");
+const qMarks = document.getElementsByClassName("question-mark");
 
 // Get the modal
-const modal = document.getElementById("qModal");
+const modals = document.getElementsByClassName("modal");
 
 // Get the <span> element that closes the modal
-const span = document.getElementsByClassName("close")[0];
+const spans = document.getElementsByClassName("close");
 
 // When the user clicks on the button, open the modal
-qMark.onclick = function() {
-    console.log("button clicked");
-    modal.style.display = "block";
-}
+[...qMarks].forEach((btn, ind) => {
+    btn.onclick = () => (modals[ind].style.display = 'block')
+});
+
+// qMark.onclick = function() {
+//     console.log("button clicked");
+//     modal.style.display = "block";
+// }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+[...spans].forEach(( span, ind) => {
+    span.onclick = () => (modals[ind].style.display = 'none');
+});
+
+// span.onclick = function() {
+//   modal.style.display = "none";
+// }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal) {
+  [...modals].forEach(( modal) => {
+        if (event.target === modal) {
     modal.style.display = "none";
   }
-} 
+  })
+}
