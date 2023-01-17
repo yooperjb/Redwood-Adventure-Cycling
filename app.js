cdda608e4a0c0c55d07aae838ca93262b72af8db
Pedal <jmb181@humboldt.edu>
2023-01-16 19:07:13 -0800

    removed jquery links

--- app.js
+++ app.js
@@ -8,29 +8,29 @@ const sequelize = require('./config/connection');
 const exphbs = require('express-handlebars');
 const helpers = require('./utils/helpers'); // import helper functions
 const passport = require('./config/passport');
-// const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 // create handlebars - pass in helper functions document
 const hbs = exphbs.create({ helpers });
-console.log("how about to here?")
+console.log("how about to here?");
 // Create a new Express application
 const app = express();
 const PORT = process.env.PORT || 443;
 
-// const sess = {
-//     secret: process.env.SESSION_SECRET,
+const sess = {
+    secret: process.env.SESSION_SECRET,
     
-//     rolling: true, // <-- Set `rolling` to `true`
-//     secureProxy: true,
-//     cookie: {
-//         httpOnly: true,
-//         maxAge: 8 * 60 * 60 * 1000 //8 hours;
-//     },
-//     resave: false,
-//     saveUninitialized: true,
-//     store: new SequelizeStore({
-//         db: sequelize
-//     })
-// };
+    rolling: true, // <-- Set `rolling` to `true`
+    secureProxy: true,
+    cookie: {
+        httpOnly: true,
+        maxAge: 8 * 60 * 60 * 1000 //8 hours;
+    },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
 
 //Configure view engine to render handlebars templates
 app.engine('handlebars', hbs.engine);
@@ -40,8 +40,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 // app.use(require('cookie-parser')()); // supposedly no longer need cookie-parser
-app.use(session({ secret: [process.env.SESSION_SECRET], resave: true, saveUninitialized: true }));
-// app.use(session(sess));
+// app.use(session({ secret: [process.env.SESSION_SECRET], resave: true, saveUninitialized: true }));
+app.use(session(sess));
 
 // Initialize Passport and restore authentication state, if any, from the session.
 app.use(passport.initialize());
