6407c763b7b6726788ea000444011e07cccd3bd4
Pedal <jmb181@humboldt.edu>
2023-02-08 21:25:22 -0800

    added checkmark to bike routes page for routes a logged-in user has ridden

--- controllers/bike-routes-routes.js
+++ controllers/bike-routes-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Routes } = require('../models');
+const { Routes, User_Routes } = require('../models');
 const sequelize = require('../config/connection');
 
 
@@ -16,10 +16,19 @@ router.get('/', async (req, res) => {
 
         )).map(route => route.toJSON()) //converts to plain object
         
-        // console.log({ routes }) //flag value
-        // console.log('routes', routes)
-        // console.log('typeof routes', typeof( routes))
-        
+        // If user signed in get routes they've ridden for route checkmark
+        if (req.user) {
+            const user_routes = (await User_Routes.findAll({
+                attributes: [
+                    'route_id'
+                ]
+            })).map(route => route.toJSON().route_id)
+            
+            routes.map(route => {
+                (user_routes.includes(route.id) ? route.ridden="ridden" : route.ridden="unridden")
+            })
+        }
+            
         // render bikeroutes page with all bike routes
         res.render('bikeroutes', { 
             title: '2023 Bike Routes',
